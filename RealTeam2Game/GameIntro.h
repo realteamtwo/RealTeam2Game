56dd6422fd24575dcdd4990b19bd472e91b35e1d
nicole <nwood4202@gmail.com>
2024-10-07 23:21:30 -0400

    added GameIntro.h and started drafting intro backstory message and controls

---
+++ RealTeam2Game/GameIntro.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+//game introduction starting with backstory, (setting, how you got here, where is it, what is your goal),
+//followed by controls so player knows what to do
+
+
+//Backstory rough draft: A teleportation spell has gone wrong and you find yourself in an unfamiliar room.
+//You must seek a way out, traveling down until you reach the ground level to flee the evil alchemist's tower!
+
+//controls rough draft: To descend the Tower, you must open the trapdoor in the middle of each room,
+//which will often be locked in some way. The method to do so is hidden in the room. Investigate,
+//solve puzzles and use items.
+//While exploring the Tower, you will find various *items* and *objects*. Such items will be helpful
+//in solving puzzles. To look at an item, type "inspect [item name]", for example "inspect torch" will
+// briefly describe what the torch looks like and if it is lit or not. To use an item, type "use [item name]"
+//for example "use torch" will light the torch while "use key" will try to use key on lock. Objects
+//can also be moved, for example "move vase".
+//to see what items are around you, type "look" and you will get an idea of what's around you including
+//stuff you can interact with.
+//once you have solved the room, type "open trapdoor" to open and descend further.
+
