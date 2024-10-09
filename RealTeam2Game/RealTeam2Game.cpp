54620b617ccacf342e75c1dbe7f3547f5b427e43
boonsackly <boonpesky@gmail.com>
2024-10-09 14:14:05 -0400

    Tested the rest of the item class.

--- RealTeam2Game/RealTeam2Game.cpp
+++ RealTeam2Game/RealTeam2Game.cpp
@@ -9,4 +9,8 @@ int main()
     Item pencil("Pencil", "A tool for writing");
     std:: cout << pencil.getDisplayName() << endl;
     std::cout << pencil.getDescription() << endl;
+    pencil.setDisplayName("Pen");
+    pencil.setDescription("A tool for wriitng in ink");
+    std::cout << pencil.getDisplayName() << endl;
+    std::cout << pencil.getDescription() << endl;
 }
