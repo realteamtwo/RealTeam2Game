649a1c9c2f245e3d457f35721f11a59a0e4bc693
numbersguy104 <numbersboy@icloud.com>
2024-10-09 21:23:16 -0400

    Very basic game loop implemented

--- RealTeam2Game/RealTeam2Game.cpp
+++ RealTeam2Game/RealTeam2Game.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "Item.h"
 #include "GameIntro.h"
+#include "PlayerInput.h"
 #include "PuzzleIdeas.h"
 #include "Room.h"
 
@@ -9,6 +10,8 @@ using namespace std;
 
 int main()
 {
+    //Test cases - commented out for now
+    /*
     Room test("Dark room", "HMMMMMMM, it's dark", 0);
     test.GetRoomName();
     test.GetRoomDescript();
@@ -21,4 +24,21 @@ int main()
     pencil.setDescription("A tool for wriitng in ink");
     std::cout << pencil.getDisplayName() << endl;
     std::cout << pencil.getDescription() << endl;
+    */
+
+    PlayerInput inputHandler;
+
+    string input;
+    while (true) {
+        cout << "Enter next command: ";
+        cin >> input;
+        if (input == "Up" || input == "Down" || input == "left" || input == "right") {
+            cout << "Command recieved" << endl;
+            inputHandler.movePlayer(input);
+        }
+        else if (input == "use" || input == "get" || input == "pickup") {
+            cout << "Command recieved" << endl;
+            inputHandler.itemInteraction(input);
+        }
+    }
 }
