b5d1bec17334d1357c43ea12c4a25dd9d59fc2c4
toddj <toddj@DESKTOP-J2A67EE>
2024-10-29 18:14:25 -0400

    Earth room leave to center room

--- RealTeam2Game/Level2.cpp
+++ RealTeam2Game/Level2.cpp
@@ -69,12 +69,17 @@ void Level2::runCenterRoom(Player &p, std::string input) {
 	}
 	else if (input == "left") {
 		cout << " you go left" << endl;
-		//p.setLocation(Level2::Location::LEFT_ROOM);
+		p.setLocation(Level2::Location::LEFT_ROOM);
 		//update location to LEFT_ROOM
 	}
 	else if (input == "right") {
 		cout << " you go right" << endl;
-		//p.setLocation(Level2::Location::RIGHT_ROOM);
+		p.setLocation(Level2::Location::RIGHT_ROOM);
+		//update location to RIGHT_ROOM
+	}
+	else if (input == "leave") {
+		cout << " Your In Center Room" << endl;
+		p.setLocation(Level2::Location::CENTER_ROOM);
 		//update location to RIGHT_ROOM
 	}
 }
@@ -85,10 +90,27 @@ void Level2::runDownRoom(Player &p, std::string input) {
 
 void Level2::runRightRoom(Player& p, std::string input) {
 	cout << "Right Room Interaction" << endl;
+	
 }
 
 void Level2::runLeftRoom(Player& p, std::string input) {
 	cout << "Left Room Interaction" << endl;
+
+	/*
+	cout << "Water Room" << endl;
+	if (input == "down") {
+		cout << "The back wall is rough and rocky. There appears to be nothing of use on this side of the room." << endl;
+	}
+	else if (input == "up") {
+		cout << "The front wall has text on it.\nI'm the third rock from the sun, with one moon as my companion\nIf you don't take care of me, who knows what'll happen" << endl;
+		cout << "I have oceans so deep and moutains so high\nI'm your home. What am I?" << endl;
+	}
+	else if (input == "left") {
+		cout << "The left wall is covered with large, spiky rocks. A chunk of one seems to have broken off onto the floor." << endl;
+	}
+	else if (input == "right") {
+		cout << "The right wall contains a large container protected by thick glass. Maybe something can break it?" << endl;
+	}*/
 }
 
 void Level2::runUpRoom(Player& p, std::string input) {
@@ -105,5 +127,10 @@ void Level2::runUpRoom(Player& p, std::string input) {
 	}
 	else if (input == "right") {
 		cout << "The right wall contains a large container protected by thick glass. Maybe something can break it?" << endl;
+	}
+
+	else if (input == "leave") {
+		cout << "You are in center room" << endl;
+		p.setLocation(Level2::Location::CENTER_ROOM);
 	}
 }
