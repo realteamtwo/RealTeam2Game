7cf55753946ea46cd2c1b9357812ab4e4a33fd3f
nicole <nwood4202@gmail.com>
2024-11-05 16:09:53 -0500

    still setting up basics

---
+++ RealTeam2Game/Level3.cpp
@@ -0,0 +1,5 @@
+//cpp for Level 3 (the potion room) will be where location descriptions snd interactions for the player will be
+#include "Level2.h"
+#include "Player.h"
+
+using namespace std;
--- RealTeam2Game/Level3.h
+++ RealTeam2Game/Level3.h
@@ -1,1 +1,13 @@
+//Header file for level 3 (The Potion Room) much like level 2. Sets up level3 class and has player class
 #pragma once
+
+#ifndef LEVEL3
+#define LEVEL3
+
+using namespace std;
+
+
+
+
+
+#endif
