faae6306f364cedb1e60b399c3e3675011bcd012
nicole <nwood4202@gmail.com>
2024-12-05 12:37:31 -0500

    made a puzzle in Left room going to on level 3

--- RealTeam2Game/Level3.cpp
+++ RealTeam2Game/Level3.cpp
@@ -3,6 +3,12 @@
 #include "Player.h"
 #include "Item.h"
 
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <vector>
+
 
 //declaring Items so they can be used in methods
 Item MagicPowder;
@@ -575,12 +581,219 @@ void Level3::runUpRoom(Player& p, std::string input) {
 	}*/
 
 }
+//for hangman in left room
+#define MAX_ATTEMPTS 6
+bool hasWon = false;
+class HangmanGame {
+public:
+	// constructor to ini
+	HangmanGame()
+	{
+		srand(static_cast<unsigned int>(time(nullptr)));
+		secretWord = "trowel";
+		currentWord = string(secretWord.length(), '_');
+		attemptsLeft = MAX_ATTEMPTS;
+	}
+
+	// function to start the game.
+	void play()
+	{
+		cout << "You find in this room a strange image made with light magic" << endl;
+		cout << "HANGMAN" << endl;
+		cout << "   _____" << endl;
+		cout << "  |     |" << endl;
+		cout << "  |      " << endl;
+		cout << "  |" << endl;
+		cout << "  |" << endl;
+		cout << "  |" << endl;
+		cout << "  |" << endl;
+
+		cout << "You have " << attemptsLeft
+			<< " attempts to guess the word."
+			<< endl;
+
+		// the main game loop which will go on till the
+		// attempts are left or the game is won.
+		while (attemptsLeft > 0) {
+			displayGameInfo();
+			char guess;
+			cout << "Guess a letter: ";
+			cin >> guess;
+
+			if (isalpha(guess)) {
+				guess = tolower(guess);
+				if (alreadyGuessed(guess)) {
+					cout << "You've already guessed that "
+						"letter."
+						<< endl;
+				}
+				else {
+					bool correctGuess = updateCurrentWord(guess);
+					// if the guess is correct, we will
+					// update the word and check if the word
+					// is completely guessed or not
+					if (correctGuess) {
+						cout << "Good guess!" << endl;
+						drawHangman(attemptsLeft);
+						// if the word is completely
+						// guessed.
+						if (currentWord == secretWord) {
+							displayGameInfo();
+							cout << "Congratulations! You "
+								"guessed the word: "
+								<< secretWord << endl;
+							hasWon = true;
+							return;
+						}
+					}
+					else {
+						cout << "Incorrect guess." << endl;
+						attemptsLeft--;
+						drawHangman(attemptsLeft);
+					}
+				}
+			}
+			else {
+				cout << "Please enter a valid letter."
+					<< endl;
+			}
+		}
+
+		if (attemptsLeft == 0) {
+			displayGameInfo();
+			cout << "You've run out of attempts." << endl;
+			cout << "restarting game" << endl;
+			attemptsLeft = 6;
+			currentWord = string(secretWord.length(), '_');
+		}
+	}
+
+private:
+	string secretWord;
+	string currentWord;
+	int attemptsLeft;
+	vector<char> guessedLetters;
+
+
+	// checking if the word is already guessed
+	bool alreadyGuessed(char letter)
+	{
+		return find(guessedLetters.begin(),
+			guessedLetters.end(), letter)
+			!= guessedLetters.end();
+	}
+
+	// updating the word after correct guess
+	bool updateCurrentWord(char letter)
+	{
+		bool correctGuess = false;
+		for (int i = 0; i < secretWord.length(); i++) {
+			if (secretWord[i] == letter) {
+				currentWord[i] = letter;
+				correctGuess = true;
+			}
+		}
+		guessedLetters.push_back(letter);
+		return correctGuess;
+	}
+
+	// function to provide the info at particular point in
+	// the game
+	void displayGameInfo()
+	{
+		cout << "Word: " << currentWord << endl;
+		cout << "Attempts left: " << attemptsLeft << endl;
+		cout << "Guessed letters: ";
+		for (char letter : guessedLetters) {
+			cout << letter << " ";
+		}
+		cout << endl;
+	}
+
+	// function to progressively draw the hangman
+	void drawHangman(int attemptsLeft)
+	{
+		// Add your hangman drawing logic here
+		// For simplicity, you can print a static hangman
+		// ASCII art Modify this function to display the
+		// hangman as you like
+		if (attemptsLeft == 6) {
+			cout << "   _____" << endl;
+			cout << "  |     |" << endl;
+			cout << "  |      " << endl;
+			cout << "  |" << endl;
+			cout << "  |" << endl;
+			cout << "  |" << endl;
+			cout << "  |" << endl;
+		}
+		else if (attemptsLeft == 5) {
+			cout << "   _____" << endl;
+			cout << "  |     |" << endl;
+			cout << "  |     O" << endl;
+			cout << "  |" << endl;
+			cout << "  |" << endl;
+			cout << "  |" << endl;
+			cout << "  |" << endl;
+		}
+		else if (attemptsLeft == 4) {
+			cout << "   _____" << endl;
+			cout << "  |     |" << endl;
+			cout << "  |     O" << endl;
+			cout << "  |     |" << endl;
+			cout << "  |" << endl;
+			cout << "  |" << endl;
+			cout << "  |" << endl;
+		}
+		else if (attemptsLeft == 3) {
+			cout << "   _____" << endl;
+			cout << "  |     |" << endl;
+			cout << "  |     O" << endl;
+			cout << "  |    /|" << endl;
+			cout << "  |" << endl;
+			cout << "  |" << endl;
+			cout << "  |" << endl;
+		}
+		else if (attemptsLeft == 2) {
+			cout << "   _____" << endl;
+			cout << "  |     |" << endl;
+			cout << "  |     O" << endl;
+			cout << "  |    /|\\" << endl;
+			cout << "  |" << endl;
+			cout << "  |" << endl;
+			cout << "  |" << endl;
+		}
+		else if (attemptsLeft == 1) {
+			cout << "   _____" << endl;
+			cout << "  |     |" << endl;
+			cout << "  |     O" << endl;
+			cout << "  |    /|\\" << endl;
+			cout << "  |    /" << endl;
+			cout << "  |" << endl;
+			cout << "  |" << endl;
+		}
+		else if (attemptsLeft == 0) {
+			cout << "   _____" << endl;
+			cout << "  |     |" << endl;
+			cout << "  |     O" << endl;
+			cout << "  |    /|\\" << endl;
+			cout << "  |    / \\" << endl;
+			cout << "  |" << endl;
+			cout << "  |" << endl;
+		}
+	}
+};
+
+
 void Level3::runLeftRoom(Player& p, std::string input) {
 	if (p.hasItem(Trowel)) {
 		cout << "You have already solved the room there is nothing left here" << endl;
 	}
 	else {
-
+		while (hasWon != true) {
+			HangmanGame game;
+			game.play();
+		}
+		cout << "You received a trowel! " << endl;
 		p.addItem(Trowel);
 	}
 	cout << "type leave to leave" << endl;
